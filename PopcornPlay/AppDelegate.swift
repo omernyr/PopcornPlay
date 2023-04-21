95f2cd343509abfd6a86ba4075bd2278e9d8bb6a
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-04-21 18:08:16 +0300

    Initial Commit

---
+++ PopcornPlay/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PopcornPlay
+//
+//  Created by macbook pro on 21.04.2023.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = MainViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+    
+}
+
---
+++ PopcornPlay/MainViewController/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  PopcornPlay
+//
+//  Created by macbook pro on 21.04.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
