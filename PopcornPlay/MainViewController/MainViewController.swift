3e393ac7adb417a430f7b394ed41e36a318a51d9
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-04-21 18:12:20 +0300

    Added NavigationVC

--- PopcornPlay/MainViewController/MainViewController.swift
+++ PopcornPlay/MainViewController/MainViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "Main View"
+        
     }
 }
