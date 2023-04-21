a456ad1228c1b069cab9feaf634342e22adceafe
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-04-21 18:49:41 +0300

    Added MainViewmodel

---
+++ PopcornPlay/ViewModel/MainViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewModel.swift
+//  PopcornPlay
+//
+//  Created by macbook pro on 21.04.2023.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        10
+    }
+    
+}
