87e2bb7dd8201a6465e5a09f9d090e59e32d48df
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-04-21 18:50:02 +0300

    Shoe rows

--- PopcornPlay/Controllers/MainViewController/MainViewController.swift
+++ PopcornPlay/Controllers/MainViewController/MainViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    
+    // IBOutlets
+    @IBOutlet var tableView: UITableView!
+    
+    // ViewModels
+    var viewModel: MainViewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Main View"
         
+        configView()
+    }
+    
+    private func configView() {
+        self.title = "Main View"
+        self.view.backgroundColor = .systemOrange
+        
+        setupTableView()
     }
 }
