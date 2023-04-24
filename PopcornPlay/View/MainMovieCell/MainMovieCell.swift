37c75f69068c2cbd65079b00a584442960cf01cf
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-04-24 19:47:16 +0300

    Added MainMovieCell

---
+++ PopcornPlay/View/MainMovieCell/MainMovieCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainMovieCell.swift
+//  PopcornPlay
+//
+//  Created by macbook pro on 24.04.2023.
+//
+
+import UIKit
+
+class MainMovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
