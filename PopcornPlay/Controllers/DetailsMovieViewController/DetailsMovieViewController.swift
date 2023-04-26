e8fa06925db27e4b84b37de4957c3de12495ff33
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-04-26 14:59:19 +0300

    Added DetailsMovieVC

---
+++ PopcornPlay/Controllers/DetailsMovieViewController/DetailsMovieViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsMovieViewController.swift
+//  PopcornPlay
+//
+//  Created by macbook pro on 26.04.2023.
+//
+
+import UIKit
+
+class DetailsMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .purple
+    }
+
+}
