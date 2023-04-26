c6bc89d0efadb89f68be0ea79839aa43bfb2e219
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-04-26 10:21:02 +0300

    Bind IBOutlets

--- PopcornPlay/View/MainMovieCell/MainMovieCell.swift
+++ PopcornPlay/View/MainMovieCell/MainMovieCell.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class MainMovieCell: UITableViewCell {
-
+    
+    // Outlets
+    @IBOutlet var backView: UIView!
+    @IBOutlet var movieImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var rateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        backView.round()
+        backView.addBorder(color: .label, width: 10.0)
+        
+        movieImageView.layer.cornerRadius = 5.0
     }
 
 }
