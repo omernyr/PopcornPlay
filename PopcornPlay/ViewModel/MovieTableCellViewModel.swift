a364baa97439c6a24316be2354034d0c3690a963
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-04-26 12:53:23 +0300

    Added MovieTableCellViewModel

--- PopcornPlay/ViewModel/MovieTableCellViewModel.swift
+++ PopcornPlay/ViewModel/MovieTableCellViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class MovieTableCellViewModel {
+    
+    var id: Int
+    var title: String
+    var date: String
+    var rating: String
+    var imageURL: URL?
+    
+    
+    init(movie: Movie) {
+        self.id = movie.id
+        self.title = movie.title ?? movie.name ?? ""
+        self.date = movie.releaseDate ?? movie.firstAirDate ?? ""
+        self.rating = "\(movie.voteAverage ?? 0)/10"
+        self.imageURL = makeImageURL(movie.posterPath ?? "")
+    }
+    
+    private func makeImageURL( _ imageCode: String) -> URL? {
+        URL(string: "\(NetworkingConstant.shared.imageServerAdrress)\(imageCode)")
+    }
+    
+}
