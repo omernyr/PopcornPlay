cefc677c4ac28db4cffbddc075854a5ef8f5aaf5
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-04-26 16:06:12 +0300

    constraints fixes and show movie details

--- PopcornPlay/Controllers/DetailsMovieViewController/DetailsMovieViewController.swift
+++ PopcornPlay/Controllers/DetailsMovieViewController/DetailsMovieViewController.swift
@@ -6,12 +6,41 @@
 //
 
 import UIKit
+import SDWebImage
 
 class DetailsMovieViewController: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    // ViewModel:
+    var viewModel: DetailsMovieViewModel
+    
+    init(viewModel: DetailsMovieViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: "DetailsMovieViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .purple
+        
+        configView()
     }
+    
+    func configView() {
+        self.view.backgroundColor = .purple
+        self.title = "Movie Details"
+        
+        self.titleLabel.text = viewModel.movieTitle
+        self.descriptionLabel.text = viewModel.movieDescription
+        self.imageView.sd_setImage(with: viewModel.movieImage)
+    }
+    
 
 }
